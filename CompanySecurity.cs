365259b27a8a0527781e8ae5fd25cc14e01bc745
lawsona7 <lawsona7@mail.uc.edu>
2016-11-27 23:15:38 -0500

    Initial Commit

---
+++ CompanySecurity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GroceryDataAccess;
+
+namespace GroceryApi
+{
+    public class CompanySecurity
+    {
+        public static bool Login(string username, string password)
+        {
+            using(GroceryDBEntities entities = new GroceryDBEntities())
+            {
+                return entities.Users.Any(user => user.Username.Equals(username, StringComparison.OrdinalIgnoreCase) 
+                && user.Password == password);
+            }
+        }
+    }
+}
